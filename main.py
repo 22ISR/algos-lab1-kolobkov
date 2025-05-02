146080a95f3e4bbc3bfa4066be20a4eaac375af6
anmagel <151705638+Vaydeee@users.noreply.github.com>
2025-05-02 12:53:42 +0600

    main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@
 hint: что такое print?
 """
 
-# Замените это своим кодом
+print ("Hello world")
 
 """
  _____         _      _____ 
@@ -27,7 +27,16 @@ hint: что такое print?
 hint: цикл, если и "%"
 """
 
-# Замените это своим кодом
+x = int(input("Введите число больше единицы"))+1
+for i in range (1, x):
+   if i % 3==0:
+     print ("Fizz")
+   elif i % 5==0:
+     print ("Buzz")
+   elif i % 3==0 and i % 5==0:
+     print ("FizzBuzz")
+   else:
+    print (i)
 
 """
  _____         _      _____ 
@@ -41,8 +50,14 @@ hint: цикл, если и "%"
 
 hint: https://ru.wikihow.com/%D0%B2%D1%8B%D1%81%D1%87%D0%B8%D1%82%D1%8B%D0%B2%D0%B0%D1%82%D1%8C-%D0%B2%D0%B8%D1%81%D0%BE%D0%BA%D0%BE%D1%81%D0%BD%D1%8B%D0%B5-%D0%B3%D0%BE%D0%B4%D1%8B
 """
+x = int(input("Введите год.."))
+if x % 4 == 0 and x % 100 == 0:
+  print("Весокосный год")
+elif x % 100 == 0 and x % 400 == 0:
+  print("Весокосный год")
+else:
+  print(x, "Весокосный год")
 
-# Замените это своим кодом
 
 """
  _____         _        ___ 
@@ -57,7 +72,12 @@ hint: https://ru.wikihow.com/%D0%B2%D1%8B%D1%81%D1%87%D0%B8%D1%82%D1%8B%D0%B2%D0
 hint: https://letpy.com/handbook/builtins/reversed/
 """
 
-# Замените это своим кодом
+x = input("Введите число или строку: ")
+y = x[::-1]
+if x == y:
+  print("Строка/число является полидромом")
+else:
+  print("Строка/число неявляется полидромом")
 
 """
  _____         _      _____ 
@@ -72,7 +92,11 @@ hint: https://letpy.com/handbook/builtins/reversed/
 hint: https://ru.wikipedia.org/wiki/%D0%A4%D0%B0%D0%BA%D1%82%D0%BE%D1%80%D0%B8%D0%B0%D0%BB
 """
 
-# Замените это своим кодом
+x = int(input("Введите факторила.."))
+factorial = 1
+for i in range (1, x+1):
+  factorial = factorial * i
+print (factorial)
 
 """
  _____         _       ____ 
@@ -88,7 +112,18 @@ hint: x <= 1 - не простые числа
 hint 2: %
 """
 
-# Замените это своим кодом
+x = int(input("Введите число.."))
+count = 0
+if x <= 1: 
+  print ("не простое")
+else:
+  for i in range (1, x+1):
+    if x % i == 0:
+      count += 1
+  if count != 2:
+    print ("не простое") 
+  else:
+    print ("простое")   
 
 """
  _____         _      ______
@@ -102,9 +137,13 @@ hint 2: %
 
 hint: циклы
 """
-
-# Замените это своим кодом
-
+x = int(input("Введите число.."))
+сумма = 0
+while x != 0:
+  a = x % 10
+  x = x // 10
+  сумма += a
+print (сумма)
 """
  _____         _      _____ 
 |_   _|       | |    |  _  |
@@ -119,8 +158,13 @@ hint: 1, 1, 2, 3 https://ru.wikipedia.org/wiki/%D0%A7%D0%B8%D1%81%D0%BB%D0%B0_%D
 hint 2: попробуйте решить с помощью рекурсии
 """
 
-# Замените это своим кодом
+def fibonacci_limit(x):
+    sequence = []
+    a, b = 0, 1
+    while a <= x:
+        sequence.append(a)
+        a, b = b, a + b
+    return sequence
 
-
-
-
+x = int(input("Введите число: "))
+print(f"Последовательность Фибоначчи до {x}: {fibonacci_limit(x)}")5
